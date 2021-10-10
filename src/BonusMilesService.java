0d307bf698be7bf4329634139472b4f7230631f9
Vorobishek <vorobishek@list.ru>
2021-10-09 23:15:15 -0700

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate (int cost) {
+        int milePrice = 20;
+        int miles;
+
+        miles = cost / milePrice;
+    return (miles);
+    }
+}
